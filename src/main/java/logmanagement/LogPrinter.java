abf0d086b49dfde3c82e3b6830b7c06d03e5bdd9
mutyala <ss599608@dal.ca>
2021-12-07 21:28:23 -0400

    Updated general for fetching different oprtaions count

--- src/main/java/logmanagement/LogPrinter.java
+++ src/main/java/logmanagement/LogPrinter.java
@@ -1,4 +1,8 @@
-package logmanagement;
+package utils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Thread.sleep;
 
@@ -13,8 +17,150 @@ public class LogPrinter {
         return instanceObject;
     }
 
+    public List<Integer> generalMessagePrinter(String messageContent, String type) throws IOException {
+
+        String message="";
+        int counter=1;
+        List<Integer> counters = new ArrayList<Integer>();
+
+        if (type.equals("table")) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/schemaDetails.txt"));
+                String data = null;
+                while ((data = br.readLine()) != null) {
+                    if (data.matches("\\[{1}\\w*\\]")) {
+                        counter++;
+                    }
+                }
+                message=messageContent+counter+" tables";
+            } catch (Exception e) {
+                message="failed to create table";
+                e.printStackTrace();
+            } finally {
+                Logger.getInstanceObject().sendDataToGeneralLogFile(message);
+                //counters.add(tableCounter);
+                br.close();
+            }
+        }
+
+        if (type.equals("insert")) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/StructureAndDataExport.txt"));
+                String data = null;
+                while ((data = br.readLine()) != null) {
+                    if (data.toLowerCase().contains("insert into")) {
+                        counter++;
+                    }
+                }
+                message=messageContent+counter+" insert statements";
+            } catch (IOException e) {
+                message="failed to insert into table";
+                e.printStackTrace();
+            } finally {
+                Logger.getInstanceObject().sendDataToGeneralLogFile(message);
+                br.close();
+            }
+        }
+
+        if (type.equals("update")) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/StructureAndDataExport.txt"));
+                String data = null;
+                while ((data = br.readLine()) != null) {
+                    if (data.toLowerCase().contains("update")) {
+                        counter++;
+                    }
+                }
+                message=messageContent+counter+" update statements";
+            } catch (IOException e) {
+                message="failed to update into table";
+                e.printStackTrace();
+            } finally {
+                Logger.getInstanceObject().sendDataToGeneralLogFile(message);
+                br.close();
+            }
+        }
+
+        if (type.equals("delete")) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/StructureAndDataExport.txt"));
+                String data = null;
+                while ((data = br.readLine()) != null) {
+                    if (data.toLowerCase().contains("delete")) {
+                        counter++;
+                    }
+                }
+                message=messageContent+counter+" delete statements";
+            } catch (IOException e) {
+                message="failed to delete record";
+                e.printStackTrace();
+            } finally {
+                Logger.getInstanceObject().sendDataToGeneralLogFile(message);
+                br.close();
+            }
+        }
+
+        if (type.equals("drop")) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/company/StructureAndDataExport.txt"));
+                String data = null;
+                while ((data = br.readLine()) != null) {
+                    if (data.toLowerCase().contains("drop")) {
+                        counter++;
+                    }
+                }
+                message=messageContent+counter+" drop statements";
+            } catch (IOException e) {
+                message="failed to drop table";
+                e.printStackTrace();
+            } finally {
+                Logger.getInstanceObject().sendDataToGeneralLogFile(message);
+                br.close();
+            }
+        }
+        return counters;
+    }
+
+
+//        int tableCounter = 0;
+//        BufferedReader br = null;
+//        try {
+//            br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/schemaDetails.txt"));
+//            String data = null;
+//            while((data = br.readLine())!=null){
+//                if(data.matches("\\[{1}\\w*\\]")){
+//                    tableCounter++;
+//                }
+//            }
+//            int insertCounter =0;
+//            br = new BufferedReader(new FileReader("5dbc98dcc983a70728bd082d1a47546e/Store/StructureAndDataExport.txt"));
+//            String insertData = null;
+//            while((insertData = br.readLine())!=null) {
+//                //if (Data.matches(Constants.DB_TABLE_NAME_INSERT_SEPARATOR_PATTERN)) {
+//                if (insertData.toLowerCase().contains("insert into")) {
+//                    insertCounter++;
+//                }
+//                }
+//            Logger.getInstanceObject().sendDataToGeneralLogFile(messageContent,count);
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally{
+//            br.close();
+//        }
+//        counters.add(insertcounter);
+//    }
+
+
     public void errorPrinter(String errorMessage) {
-        System.err.print("\n" + errorMessage +"\n");
+
         Logger.getInstanceObject().sendDataToEventLogFile("Error: " + errorMessage);
         try {
             sleep(500);
@@ -23,13 +169,8 @@ public class LogPrinter {
         }
     }
 
-    public void generalMessagePrinter(String messageContent) {
-        //System.out.print("\n" + messageContent +"\n");
-        Logger.getInstanceObject().sendDataToGeneralLogFile(messageContent);
-        // Logger.getInstanceObject().sendDataToEventLogFile(messageContent);
-    }
-
     public void queryMessagePrinter(String messageContent) {
+        //System.out.println("Write to query log");
         Logger.getInstanceObject().sendDataToQueryLogFile(messageContent);
     }
 
--- src/main/java/logmanagement/Logger.java
+++ src/main/java/logmanagement/Logger.java
@@ -1,4 +1,4 @@
-package logmanagement;
+package utils;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -10,23 +10,24 @@ import java.util.Date;
 public class Logger {
 
     private static Logger instanceObject = null;
-    private static String previousDate = "";
-    private static boolean eventLogDate = false;
-    private static boolean generalLogDate = false;
-    private static boolean queryLogDate = false;
+    private static String previouslyRecordedDate = "";
+    private static boolean logDateForEvent = false;
+    private static boolean logDateForGeneral = false;
+    private static boolean logDateForQuery = false;
 
     private Logger() {
         logFilesGenerator();
     }
 
     public static Logger getInstanceObject() {
+
         if (instanceObject == null) {
             instanceObject = new Logger();
         }
         return instanceObject;
     }
 
-    public static String generalLogFilePath ="src/main/java/com/logs/GeneralLogs.txt";
+    public static String generalLogFilePath = "src/main/java/com/logs/GeneralLogs.txt";
     public static String eventLogFilePath = "src/main/java/com/logs/EventLogs.txt";
     public static String queryLogFilePath = "src/main/java/com/logs/QueryLogs.txt";
     final LogPrinter printer = LogPrinter.getInstanceObject();
@@ -38,27 +39,17 @@ public class Logger {
     private void logFilesGenerator() {
 
         try {
-            File generalLogFile = new File(generalLogFilePath);  //default general log text file
-            File eventLogFile = new File(eventLogFilePath); //default event log text file
-            File queryLogFile = new File(queryLogFilePath);  //default query log text file
-            // if (generalLogFile.createNewFile())   //if no file exists, we create one
+            File generalLogFile = new File(generalLogFilePath);
+            File eventLogFile = new File(eventLogFilePath);
+            File queryLogFile = new File(queryLogFilePath);
+
             generalLogFile.createNewFile();
-//            {
-//                System.out.println("Latest General Logs are updated!");
-//            }
-            //else if(eventLogFile.createNewFile())   //if no file exists, we create one
             eventLogFile.createNewFile();
-//            {
-//                System.out.println("Latest Event Logs are updated!");
-//            }
-            //else if(queryLogFile.createNewFile())  //if no file exists, we create one
             queryLogFile.createNewFile();
-//            {
-//                System.out.println("Latest Query Logs are updated!");
-//            }
-            generalLog = new FileWriter(generalLogFile, true);//true means appending
+
+            generalLog = new FileWriter(generalLogFile, true);//It will append if it's true.
             eventLog = new FileWriter(eventLogFile, true);
-            queryLog = new FileWriter(queryLogFile,true);
+            queryLog = new FileWriter(queryLogFile, true);
         } catch (Exception e) {
             printer.errorPrinter(e.getMessage());
         }
@@ -66,29 +57,38 @@ public class Logger {
 
     private String getPresentDate() {
 
-        Date presentDate = new Date();
-        Timestamp timestamp = new Timestamp(presentDate.getTime());
         SimpleDateFormat Format = new SimpleDateFormat("MMM/dd/YYYY - HH:mm:ss - EEE");
-        String dateTimeDay = Format.format(timestamp);
-        dateTimeDay = "\n(| "+dateTimeDay+" |)\n";
-        return dateTimeDay;
+        String dateTimeDay = Format.format(new Timestamp((new Date()).getTime()));
+        return "\t\n| " + dateTimeDay + " |\n";
     }
 
+//    private void logData(String inputData) {
+//        try {
+//            String date = getPresentDate();
+//            if (date.equals(previouslyRecordedDate) && logDateForQuery)
+//                date = "";
+//            else
+//                previouslyRecordedDate = date;
+//            previouslyRecordedDate = date;
+//            queryLog.write(date + "\n" + inputData + "\n\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+//            logDateForQuery = true;
+//            queryLog.flush();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     public void sendDataToGeneralLogFile(String inputData) {
-
         try {
+            //System.out.println("Inside write general log");
             String date = getPresentDate();
-
-            if(date.equals(previousDate) && generalLogDate){
-                date =  "";
-            }
-            else{
-                previousDate = date;
-            }
-            generalLogDate = true;
-            generalLog.write(date + "\n");
-            generalLog.write(inputData + "\n");
-            generalLog.write("\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            if (date.equals(previouslyRecordedDate) && logDateForGeneral)
+                date = "";
+            else
+                previouslyRecordedDate = date;
+            previouslyRecordedDate = date;
+            generalLog.write(date + "\n" + inputData + "\n\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            logDateForGeneral = true;
             generalLog.flush();
         } catch (Exception e) {
             e.printStackTrace();
@@ -98,18 +98,13 @@ public class Logger {
     public void sendDataToEventLogFile(String inputData) {
         try {
             String date = getPresentDate();
-
-            if(date.equals(previousDate) && eventLogDate){
-                date =  "";
-            }
-            else{
-                previousDate = date;
-            }
-            previousDate = date;
-            eventLogDate = true;
-            eventLog.write(date + "\n");
-            eventLog.write(inputData+ "\n");
-            eventLog.write("\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            if (date.equals(previouslyRecordedDate) && logDateForEvent)
+                date = "";
+            else
+                previouslyRecordedDate = date;
+            previouslyRecordedDate = date;
+            eventLog.write(date + "\n" + inputData + "\n\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            logDateForEvent = true;
             eventLog.flush();
         } catch (Exception e) {
             e.printStackTrace();
@@ -119,21 +114,17 @@ public class Logger {
     public void sendDataToQueryLogFile(String inputData) {
         try {
             String date = getPresentDate();
-
-            if(date.equals(previousDate) && queryLogDate){
-                date =  "";
-            }
-            else{
-                previousDate = date;
-            }
-            previousDate = date;
-            queryLogDate = true;
-            queryLog.write(date + "\n");
-            queryLog.write(inputData + "\n");
-            queryLog.write("\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            if (date.equals(previouslyRecordedDate) && logDateForQuery)
+                date = "";
+            else
+                previouslyRecordedDate = date;
+            previouslyRecordedDate = date;
+            queryLog.write(date + "\n" + inputData + "\n\n---------------------------------------------+++++++++++++++++++++++++++---------------------------------------------\n");
+            logDateForQuery = true;
             queryLog.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
